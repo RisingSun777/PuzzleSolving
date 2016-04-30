bc600320f185ea698a699097afa7efa3a3b10af1
Duong Nguyen <superduong528@outlook.com>
2016-04-30 13:09:57 +0700

    First commit

---
+++ ProblemSolving/BloodyText.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    public class BloodyText
+    {
+        public class Input
+        {
+            public string EncryptedText { get; set; }
+            public string Dictionary { get; set; }
+            public List<char[]> Substitutions { get; set; }
+        }
+        
+        public Dictionary<char, List<int>> GetEncryptedTextcharacterIndexes(string encryptedText)
+        {
+            Dictionary<char, List<int>> ret = new Dictionary<char, List<int>>();
+
+            for (var i = 0; i < encryptedText.Length; ++i)
+                if (!ret.ContainsKey(encryptedText[i]))
+                    ret.Add(encryptedText[i], new List<int> { i });
+                else
+                    ret[encryptedText[i]].Add(i);
+
+            return ret;
+        }
+
+        public char[] Solve(char[] encryptedTextUsingSubstitutions, string dictionary, List<char[]> substitutions)
+        {
+            var explodedDict = dictionary.Split(' ').GroupBy(a => a.Length);
+
+            int bookmarkIndex = 0;
+
+            for (int i = 0; i <= encryptedTextUsingSubstitutions.Length; ++i)
+            {
+                if (i != encryptedTextUsingSubstitutions.Length && encryptedTextUsingSubstitutions[i] != ' ')
+                    continue;
+
+                int textLength = i - bookmarkIndex;
+
+                var dictionaryWithAppropriateLength = explodedDict.Single(a => a.Key == textLength);
+                
+                
+                List<int> processedIndexes = new List<int>();
+                //for (int j = 0; j < textLength; ++j)
+                //{
+                //    if (!processedIndexes.Contains(j) && ReplaceWithSubstitutions(encryptedTextUsingSubstitutions, bookmarkIndex + j, substitutions))
+                //    {
+                //        processedIndexes.Add(j);
+                //        continue;
+                //    }
+
+                //    foreach (string dictText in dictionaryWithAppropriateLength)
+                //    {
+                //        tempArr[j] = dictText[j];
+                //    }
+                //}
+                
+                foreach (string dictText in dictionaryWithAppropriateLength)
+                {
+                    int counter = 0;
+
+                    for (int j = 0; j < textLength; ++j)
+                    {
+                        if (!processedIndexes.Contains(j) && ReplaceWithSubstitutions(encryptedTextUsingSubstitutions, bookmarkIndex + j, substitutions))
+                        {
+                            processedIndexes.Add(j);
+
+                            if (dictText[j] == encryptedTextUsingSubstitutions[bookmarkIndex + j])
+                            {
+                                ++counter; 
+                                continue;
+                            }
+                            else
+                                break;
+                        }
+
+                        if (processedIndexes.Contains(j) && dictText[j] != encryptedTextUsingSubstitutions[bookmarkIndex + j])
+                            break;
+
+                        ++counter;
+                    }
+
+                    if (counter == textLength)
+                    { 
+                        for (int j = 0; j < textLength; ++j)
+                        {
+                            if (processedIndexes.Contains(j))
+                                continue;
+
+                            encryptedTextUsingSubstitutions[bookmarkIndex + j] = dictText[j];
+                        }
+
+                        break;
+                    }
+                }
+
+
+
+                bookmarkIndex = i + 1;
+            }
+
+            return encryptedTextUsingSubstitutions;
+        }
+
+        private bool ReplaceWithSubstitutions(char[] input, int replacingIndex, List<char[]> subs)
+        {
+            foreach (char[] sub in subs)
+            {
+                if (sub[0] != input[replacingIndex])
+                    continue;
+
+                input[replacingIndex] = sub[1];
+                return true;
+            }
+
+            return false;
+        }
+       
+        [Obsolete]
+        public string Solve(Input input)
+        {
+            string ret = null;
+            string[] explodedText = input.EncryptedText.Split(' ');
+
+            Dictionary<char, List<int>> encryptedTextcharacterIndexes = GetEncryptedTextcharacterIndexes(input.EncryptedText);
+            char[] encryptedTextInCharArray = input.EncryptedText.ToCharArray();
+
+            foreach (char[] substitution in input.Substitutions)
+            {
+                if (substitution.Length == 2)
+                    if (encryptedTextcharacterIndexes.ContainsKey(substitution[0]))
+                        foreach (int index in encryptedTextcharacterIndexes[substitution[0]])
+                            encryptedTextInCharArray[index] = substitution[1];
+
+
+            }
+
+            char[] alreadyProcessedChars = input.Substitutions.Select(a => a[1]).ToArray();
+
+            //ret = new string(VerifyWithDictionary(
+            //    encryptedTextInCharArray,
+            //    input.Dictionary,
+            //    alreadyProcessedChars));
+            
+            return ret;
+        }
+    }
+}
---
+++ ProblemSolvingTest/UnitTest1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProblemSolving;
+
+namespace ProblemSolvingTest
+{
+    [TestClass]
+    public class BloodyTextTest
+    {
+        private BloodyText bloodyText;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            bloodyText = new BloodyText();
+        }
+
+        [TestMethod]
+        public void GetEncryptedTextcharacterIndexes()
+        {
+            string input = "ab fghhi ba";
+
+            var ret = bloodyText.GetEncryptedTextcharacterIndexes(input);
+
+            Assert.AreEqual(input.Distinct().Count(), ret.Count);
+
+            Assert.AreEqual(0, ret['a'][0]);
+            Assert.AreEqual(10, ret['a'][1]);
+
+            Assert.AreEqual(1, ret['b'][0]);
+            Assert.AreEqual(9, ret['b'][1]);
+
+            Assert.AreEqual(3, ret['f'][0]);
+
+            Assert.AreEqual(4, ret['g'][0]);
+
+            Assert.AreEqual(5, ret['h'][0]);
+            Assert.AreEqual(6, ret['h'][1]);
+
+            Assert.AreEqual(7, ret['i'][0]);
+        }
+
+        [TestMethod]
+        public void Solve_for_2_params()
+        {
+            BloodyText.Input input = new BloodyText.Input()
+            {
+                EncryptedText = "pgfqp td c bt bh dhschddg",
+                Substitutions = new List<char[]>
+                {
+                    new[] { 'b', 'a' },
+                    new[] { 'c', 'i' },
+                    new[] { 'd', 'e' },
+                    new[] { 'f', 'u' },
+                    new[] { 'g', 'r' },
+                    new[] { 'h', 'n' },
+                    new[] { 'p', 't' },
+                    new[] { 'q', 's' },
+                    new[] { 't', 'm' },
+                    new[] { 's', 'g' },
+                }
+            };
+
+            string ret = bloodyText.Solve(input);
+
+            Assert.AreEqual("trust me i am an engineer", ret);
+        }
+
+        [TestMethod]
+        public void Solve_for_2_params_with_missing_substitution_and_have_to_look_up_dictionary()
+        {
+            BloodyText.Input input = new BloodyText.Input()
+            {
+                EncryptedText = "pgfqp td c bt bh dhschddg",
+                Substitutions = new List<char[]>
+                {
+                    new[] { 'b', 'a' },
+                    new[] { 'c', 'i' },
+                    new[] { 'd', 'e' },
+                    new[] { 'f', 'u' },
+                    new[] { 'g', 'r' },
+                    new[] { 'h', 'n' },
+                    new[] { 'p', 't' },
+                    new[] { 'q', 's' },
+                    new[] { 't', 'm' },
+                },
+                Dictionary = "rage am holy an fairy engine tale engineer i le me my trust yes thunder truth oh april goddess beach please godzilla neo matrix mogu al capone you know tired of this sheet"
+            };
+
+            char[] ret = bloodyText.Solve(input.EncryptedText.ToCharArray(), input.Dictionary, input.Substitutions);
+            Assert.AreEqual("trust me i am an engineer", new string(ret));
+
+            //string ret = bloodyText.Solve(input);
+            //Assert.AreEqual("trust me i am an engineer", ret);
+        }
+    }
+}
